dd94d4a011a3355321f8c305dd2969d369ccbab8
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2024-02-10 19:01:44 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -9,7 +9,8 @@ Config.AvailableJobs = {                                     -- Only used when n
     ['reporter'] = { ['label'] = 'News Reporter', ['isManaged'] = false },
     ['garbage'] = { ['label'] = 'Garbage Collector', ['isManaged'] = false },
     ['bus'] = { ['label'] = 'Bus Driver', ['isManaged'] = false },
-    ['hotdog'] = { ['label'] = 'Hot Dog Stand', ['isManaged'] = false }
+    ['hotdog'] = { ['label'] = 'Hot Dog Stand', ['isManaged'] = false },
+    ['postal'] = { ['label'] = 'GoPostal', ['isManaged'] = false }
 }
 
 Config.Cityhalls = {
@@ -45,7 +46,7 @@ Config.Cityhalls = {
 Config.DrivingSchools = {
     { -- Driving School 1
         coords = vec3(240.3, -1379.89, 33.74),
-        showBlip = true,
+        showBlip = false,
         blipData = {
             sprite = 225,
             display = 4,
@@ -79,7 +80,7 @@ Config.Peds = {
         model = 'a_m_m_eastsa_02',
         coords = vec4(240.91, -1379.2, 32.74, 138.96),
         scenario = 'WORLD_HUMAN_STAND_MOBILE',
-        drivingschool = true,
+        drivingschool = false,
         zoneOptions = { -- Used for when UseTarget is false
             length = 3.0,
             width = 3.0
